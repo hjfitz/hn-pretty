706ff15a86311c36fb6be88bfa8e271484172e2b
Harry Fitzgerald <hfitz83@gmail.com>
2016-11-03 19:24:44 +0000

    added function. puts nodes on page!

--- index.js
+++ index.js
@@ -24,19 +24,52 @@ function getThread(url) {
   xhr.onload = function() {
     var data = xhr.responseText;
     var parsedData = JSON.parse(data);
-    console.log(parsedData);
-    parseData();
+    //console.log(parsedData);
+    parseData(parsedData);
   };
   xhr.open("GET", url, false);
   xhr.send();
 }
 
-function parseData() {
-
+function parseData(data) {
+  var title = data.title;
+  var url = data.url;
+  var score = parseInt(data.score);
+  var added = parseInt(data.time);
+  var dateAdded = new Date(added);
+  //dateAdded = dateAdded.setUTCSeconds(added);
+  //console.log(dateAdded);
+  //console.log(title);
+  addToPage(title,url,score,dateAdded);
 }
 
-function addToPage(data) {
-  
+function addToPage(title,link,score,time) {
+  var heading          = document.createElement("h1");
+  var newLink          = document.createElement("a");
+  var postScore        = document.createElement("p");
+  var timeAdded        = document.createElement("p");
+
+  var postContainer    = document.createElement("div");
+  //var container        = window.container;
+
+  postContainer.id     = "post";
+  newLink.className   += "link";
+  postScore.className += "score";
+  timeAdded.className += "time";
+
+  heading.textContent = title;
+  newLink.href = link;
+  newLink.textContent = "link";
+  postScore.textContent = score;
+  timeAdded.textContent = time;
+
+  postContainer.appendChild(heading);
+  postContainer.appendChild(newLink);
+  postContainer.appendChild(postScore);
+  postContainer.appendChild(timeAdded);
+
+  window.container.appendChild(postContainer);
+  console.log("oioi");
 }
 
 window.addEventListener("load", getHN());
